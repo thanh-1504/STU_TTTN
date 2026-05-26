e8eabe04f2f956af62c3a98c671e974dd53ab9a3
nhatthanh <duongnhatthanhaz09@gmail.com>
2026-05-26 21:57:07 +0700

    Fix vite config

--- motorbike_fe/vite.config.js
+++ motorbike_fe/vite.config.js
@@ -1,12 +1,7 @@
-import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
+  plugins: [react(), tailwindcss()],
 });
