caa4fa7a8d6435c46029b7066a9a04ea8cd8348a
nhatthanh <duongnhatthanhaz09@gmail.com>
2026-05-27 11:31:30 +0700

    Change path

--- motorbike_fe/src/api/axios.js
+++ motorbike_fe/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
- const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-// const API_URL = import.meta.env.VITE_API_URL || "https://stu-tttn.onrender.com";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+ const API_URL = import.meta.env.VITE_API_URL || "https://stu-tttn.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
