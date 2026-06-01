a8a4e1cdcd34a2dfaaa8575aa26905c6186349ad
nhatthanh <duongnhatthanhaz09@gmail.com>
2026-06-01 12:45:00 +0700

    Change path

--- motorbike_be/src/main.ts
+++ motorbike_be/src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ extended: true, limit: '10mb' }));
   app.enableCors({
-    //origin: 'https://shop2banh.netlify.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://shop2banh.netlify.app',
+    //origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
